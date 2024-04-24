12f3e08dfa9e161518ce8d7f8c5fd340126a879d
adarsh3699 <adarsh3699@gmail.com>
2024-04-24 22:08:35 +0530

    renamed

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -1,36 +1,31 @@
-import React from "react";
+import React from 'react';
 
-import search_Icon from "./search_Icon.svg";
+import search_Icon from './search_Icon.svg';
 
-import "./navbar.css";
+import './navbar.css';
 
-const Navbar = ({
-  handleSearch,
-  resetToHomePage,
-  searchText,
-  setSearchText,
-}) => {
-  return (
-    <div className="navbar">
-      <div className="navbar__logo" onClick={resetToHomePage}>
-        Assign<span>ment</span>
-      </div>
-      <form className="navbar__search" onSubmit={handleSearch}>
-        <input
-          type="text"
-          className="navbar__search_input"
-          name="searchBox"
-          autoComplete="off"
-          value={searchText}
-          onChange={(e) => setSearchText(e.target.value)}
-          placeholder="Search"
-        />
-        <button className="navbar__search_button">
-          <img src={search_Icon} alt="search" />
-        </button>
-      </form>
-    </div>
-  );
+const Navbar = ({ handleSearch, resetToHomePage, searchText, setSearchText }) => {
+	return (
+		<div className="navbar">
+			<div className="navbar__logo" onClick={resetToHomePage}>
+				Photo<span>pedia</span>
+			</div>
+			<form className="navbar__search" onSubmit={handleSearch}>
+				<input
+					type="text"
+					className="navbar__search_input"
+					name="searchBox"
+					autoComplete="off"
+					value={searchText}
+					onChange={(e) => setSearchText(e.target.value)}
+					placeholder="Search"
+				/>
+				<button className="navbar__search_button">
+					<img src={search_Icon} alt="search" />
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default Navbar;
